f557c9b556c8a2678f8bea82a995000bb056a523
Deathlyjoy <colombelle.quentin@icloud.com>
2023-11-06 07:43:12 +0100

    Add Footer

--- src/App.js
+++ src/App.js
@@ -1,48 +1,32 @@
-import Card from './components/Card';
-import Experience from './components/Experience';
-import Skills from './components/Skills';
+//---------- REACT ----------
 import React, { Component } from "react";
-import Modal from "react-modal";
-import "./App.scss";
-import "./scss/modal.scss";
+//import Modal from "react-modal";
+
+//---------- COMPONENTS ----------
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import ServiceCards from "./components/ServicesCards";
 
-import data from "./data/service_french.json";
+//---------- CSS ----------
+import "./scss/App.scss";
+
+//---------- JSON ----------
 import sharedData from './data/portfolio_shared_data.json';
-import language from "./data/res_language.json"
 
-Modal.setAppElement("#root");
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      resumeData: data,
+      resumeData: {},
       sharedData: sharedData,
-      languagedata: language
     };
   }
 
   render() {
     return (
       <div>
-        <Header sharedData={this.state.sharedData.basic_info} />
-        <About
-          resumeBasicInfo={this.state.resumeData.basic_info}
-          sharedBasicInfo={this.state.sharedData.basic_info}
-        />
-        <Projects resumeProjects={this.state.languagedata.projects} />
-        <ServiceCards services={data.services} />
-        {/* Add other components here */}
-        <Skills />
-        <About resumeBasicInfo={this.state.languagedata.basic_info} />
-        <Experience resumeExperience={this.state.languagedata.experience} />
-        <Card />
-        <Footer sharedBasicInfo={this.state.sharedData.basic_info} />
+        <Header sharedData={this.state.sharedData.basic_info}/>
+        <Footer sharedBasicInfo={this.state.sharedData.basic_info}/>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
+import ReactDOM from 'react-dom/client';
+import './scss/index.scss';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 /* GLOBAL VARIABLES */
-
 window.$secondaryLanguage = 'fr';
-window.$secondaryLanguageIconId = 'secondary-lang-icon';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.register();
